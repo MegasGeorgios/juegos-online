56b515adcfbda441ec9dc834496e8d6b59a64f10
georgiosbr <georgios.botas@corusconsulting.com>
2020-03-09 17:06:35 +0100

    ajax json and background color opt validate question

--- js/pre-resp.js
+++ js/pre-resp.js
@@ -1,21 +1,23 @@
 (function($) {
 
 /**VARIABLES GLOBALES**/
-var fileContent;
-var countQt = 1;
+var arrQtRes;
+var countQt = 0;
 
 /**FUNCIONES**/
 
 /*
 *	Obtener mediante ajax las preguntas y respuestas.
 */
-function requestData() 
+function requestDataJSON() 
 {
+	console.log('ejecutando funcion');
 	$.ajax({
-	    url: "./pre-resp.txt",
+		dataType: "json",
+	    url: "../assets/ajax/preg-resp.json",
 	    async: false,
 	    success: function (data){
-	        fileContent = data;
+	        arrQtRes = data.arrData;//JSON.parse(data.arrData);
 	    }
 	});
 }
@@ -23,15 +25,14 @@ function requestData()
 /*
 *	Pintar pregunta con sus opciones de respuesta 
 */
-function generateQuestion(arrQtRes, numQt)
+function printQuestion(numQt)
 {
-	var qt = arrQtRes[numQt].split("#"); // la pregunta
-	var opt = qt[1].split(","); // las opciones de respuesta
-	qt = qt[0];
+	var obj = arrQtRes[numQt]; 
+	var qt = obj.qt;	// la preguta
+	var opt = obj.opt; // las opciones de respuesta
+	var optCorrect = obj.optCorrect; //respuesta correcta
 
-	console.log(arrQtRes[1]);
-	console.log(opt);
-	$("#qt").text('¿'+qt); // pintar pregunta
+	$("#qt").text(qt); // pintar pregunta
 
 	for (let i=0; i < 4; i++) 
 	{
@@ -39,22 +40,59 @@ function generateQuestion(arrQtRes, numQt)
 		let selector = '#opt-'+str;
 		$(selector).text(opt[i]); // pintar opcion de respuesta
 	}
+
+	return optCorrect;
 }
 
+/*
+*	Validar si la opt seleccionada es la correcta 
+*/
+function validateOpt(elem, optCorrect)
+{
+	elem.removeClass("list-group-item-dark");
+
+	if ( optCorrect == elem.text() ) 
+	{
+		elem.addClass("list-group-item-success");
+	}else{
+		elem.addClass("list-group-item-danger");
+		
+		$(".option").each(function( index) 
+		{
+			if ($(this).text() == optCorrect) 
+			{
+				$(this).addClass("list-group-item-success");
+			}
+		});
+	}
+}
 
 /**jQuery**/
 $( document ).ready(function() {
 
-	requestData();
+	requestDataJSON();
 
-	var arrQtRes = fileContent.split("¿");
-
-	// generar primera pregunta
-	generateQuestion(arrQtRes, countQt);
+	// pintar primera pregunta con sus opciones y obtener respuesta correcta
+	var optCorrect = printQuestion(0);
 
 	$(".option").unbind().click(function() {
 
+		var elem = $(this);
+		// una vez seleccionada una opcion, deshabilitamos el resto
+		// y cambiamos el color de fondo de la opcion seleccionada
+		$(".option").prop('disabled', true);
+		elem.addClass("list-group-item-dark");
 
+		setTimeout(function()
+		{	
+			validateOpt(elem, optCorrect);
+
+		}, 1000);
+
+		// contador siguiente pregunta.
+		countQt++;
+
+		
 
 	});
 	   
