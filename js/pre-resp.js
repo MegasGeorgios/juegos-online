4228cd155d1881c07ebdc7168e71e391c9102b34
georgiosbr <georgios.botas@corusconsulting.com>
2020-03-10 17:36:41 +0100

    ramdon order opt

--- js/pre-resp.js
+++ js/pre-resp.js
@@ -1,7 +1,8 @@
 (function($) {
 
 /**VARIABLES GLOBALES**/
-var arrQtRes;
+var arrQtResJSON;
+var arrQtRes = [];
 var countQt = 0;
 var success = 0;
 
@@ -17,7 +18,7 @@ function requestDataJSON()
 	    url: "../assets/ajax/preg-resp.json",
 	    async: false,
 	    success: function (data){
-	        arrQtRes = data.arrData;//JSON.parse(data.arrData);
+	        arrQtResJSON = data.arrData;//JSON.parse(data.arrData);
 	    }
 	});
 }
@@ -25,7 +26,7 @@ function requestDataJSON()
 /*
 *	Pintar pregunta con sus opciones de respuesta 
 */
-function printQuestion(numQt)
+function printQuestion(arrQtRes, numQt)
 {
 	// eliminar clases resp correcta o erronea
 	$(".option").removeClass("list-group-item-success");
@@ -35,6 +36,7 @@ function printQuestion(numQt)
 	var obj = arrQtRes[numQt]; 
 	var qt = obj.qt;	// la preguta
 	var opt = obj.opt; // las opciones de respuesta
+	opt = randomOrder(opt);
 	var optCorrect = obj.optCorrect; //respuesta correcta
 
 	$("#qt").text(qt); // pintar pregunta
@@ -47,6 +49,47 @@ function printQuestion(numQt)
 	}
 
 	return optCorrect;
+}
+
+function randomOrder(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+/*
+*	Generar aleatoriamente las 10 preguntas 
+*/
+function getTenQt()
+{
+	var arrTenQt = [];
+	var arrIndex = [];
+
+	for (let i=0; i < 50; i++) 
+	{
+		arrIndex.push(i);
+	}
+
+	// array de indices aleatorios sin repetir 
+	arrIndex = arrIndex.sort(function() {return Math.random() - 0.5});
+
+	for (let i=0; i < 10; i++) 
+	{
+		arrTenQt.push(arrQtResJSON[arrIndex[i]]);
+	}
+	
+	return arrTenQt;
 }
 
 /*
@@ -73,14 +116,22 @@ function validateOpt(elem, optCorrect)
 	}
 }
 
+/*
+*	Generar indice aleatorio
+*/
+function getRandomIndex(min, max)
+{
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 /**jQuery**/
 $( document ).ready(function() {
 
 	requestDataJSON();
+	arrQtRes = getTenQt();
 
 	// pintar primera pregunta con sus opciones y obtener respuesta correcta
-	var optCorrect = printQuestion(0);
+	var optCorrect = printQuestion(arrQtRes, 0);
 
 	$(".option").unbind().click(function() {
 
@@ -105,7 +156,7 @@ $( document ).ready(function() {
 			if (countQt < 10)
 			{
 				// siguiente pregunta
-				optCorrect = printQuestion(countQt);
+				optCorrect = printQuestion(arrQtRes,countQt);
 			}else{
 				alert('¡¡¡ Has acertado '+success+' de 10 !!!');
 			}
